73f3a79e88c8cdb9498f9c56ddff31e309d1e5c6
Artem Ivantsov <ivantsovartem@outlook.com>
2020-08-01 14:31:48 +0300

    Sln

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CouchbaseDI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public WeatherForecastController(ICustomBucketProvider namedBucketProvider,
+            ILogger<WeatherForecastController> logger)
+        {
+            _namedBucketProvider = namedBucketProvider;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+
+        private static readonly string[] Summaries =
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ICustomBucketProvider _namedBucketProvider;
+        private readonly ILogger<WeatherForecastController> _logger;
+    }
+}
